90598eab9b0d86b7ccf764873527e89c60e9c351
ryan <ryantxu@gmail.com>
2019-05-20 15:09:14 -0700

    update gui package

--- build/prebuild.sh
+++ build/prebuild.sh
@@ -11,7 +11,7 @@ if [ ! -d $TARGETDIR ]; then
   echo "Copy grafana ui $TARGETDIR"
   mkdir $TARGETDIR
   cd $TARGETDIR
-  wget https://gist.github.com/ryantxu/2f38b39a8a1f8441b0330a4bde34b3fd/raw/394c1d1d0977c433f5c085f444bd5cd2e38c5538/grafana-ui-6.3pre.zip
+  wget https://gist.github.com/ryantxu/2f38b39a8a1f8441b0330a4bde34b3fd/raw/e2dc881dd47176131aee5309b21cf12ebc8af134/grafana-ui-6.3pre.zip
   unzip grafana-ui-6.3pre.zip
   cd ../..
 else
